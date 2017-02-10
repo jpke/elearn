de1f677868b5b1b2c2e0e8aa824a0d6cf8b1131c
jpke <j.patrick.earnest@gmail.com>
2017-02-10 02:46:44 -0500

    trying to map radial button state into component

--- src/containers/EditQuizItemsContainer.js
+++ src/containers/EditQuizItemsContainer.js
@@ -1,14 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {createQuizView, createQuiz, addItem, editQuiz} from '../actions/eLearnActions';
 import EditQuizItemView from '../components/EditQuizItemView';
 import EditQuizTitleView from '../components/EditQuizTitleView';
 import seedItem from '../utils/seedItem';
 
-export const EditQuizItemsContainer = (props) => {
-  console.log("edit items cont ", props);
+class EditQuizItemsContainer extends Component {
+  constructor(props) {
+    super(props)
+    let items = props.items.slice();
+    if(items == false) {
+      items = [seedItem];
+    }
+    this.state = {
+      correct: this.props.items.map((item, index) => {
+          return item.answers.map((answer, idx) => {
+            let id = index + " " + idx;
+            return {id: id, correct: answer.correct};
+          })
+        })
+      ,
+      items: items.map((item, index) => {
+      return (
+        <EditQuizItemView
+          key={index}
+          question={item.question}
+          answers={item.answers}
+          index={index}
+          editQuizItem={this.editQuizItem.bind(this)}
+          deleteAnswer={this.deleteAnswer.bind(this)}
+          addAnswer={this.addAnswer.bind(this)}
+        />
+      );
+    });
+  }
+  }
+  componentWillReceiveProps(nextProps) {
+      console.log("edit items cont ", props);
+    nextProps.items.forEach((item, index) => {
+      item.answers.forEach((answer, idx) => {
+        if(answer.correct != this.props.items[index].answers[idx].correct) {
+          let id = index + " " + idx;
+          this.setState({
+            state: state.slice(0,index).concat({id: id, correct: answer.correct}).slice(index + 1, state.length)
+          });
+        }
+      })
+    })
+  }
 
-  const editQuizItem = (event) => {
+  editQuizItem(event) {
     event.preventDefault();
     console.log("target: ", event.target);
     const target = event.target.id.split(" ");
@@ -22,46 +63,28 @@ export const EditQuizItemsContainer = (props) => {
 
   };
 
-  const deleteAnswer = (event) => {
+  deleteAnswer(event) {
     console.log(event.target);
     // answers = answers.slice(0,answerIndex).concat(answers.slice(answerIndex+1,answers.length));
   };
 
-  const addAnswer = (event) => {
+  addAnswer(event) {
     console.log(event.target);
     const newAnswer = {answer: "Add answer here", correct: false};
     // answers.push(newAnswer);
   }
-
-  let items = props.items.slice();
-  if(items == false) {
-    items = [seedItem];
-  }
-  items = items.map((item, index) => {
+  render() {
     return (
-      <EditQuizItemView
-        key={index}
-        question={item.question}
-        answers={item.answers}
-        index={index}
-        editQuizItem={editQuizItem}
-        deleteAnswer={deleteAnswer}
-        addAnswer={addAnswer}
-      />
+      <div>
+        <EditQuizTitleView
+          title={props.title}
+          minimumScore={props.minimumScore}
+          editQuizItem= {this.editQuizItem.bind(this)}
+        />
+      {items}
+      </div>
     );
-  });
-
-  console.log("items into render: ", items);
-  return (
-    <div>
-      <EditQuizTitleView
-        title={props.title}
-        minimumScore={props.minimumScore}
-        editQuizItem= {editQuizItem}
-      />
-    {items}
-    </div>
-  );
+  }
 };
 
 // QuizPage.propTypes
