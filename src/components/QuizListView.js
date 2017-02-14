36778e0f8af396b3cf49114e728acba279b4a123
jpke <j.patrick.earnest@gmail.com>
2017-02-14 15:01:12 -0500

    check mark for passed quizzes

--- src/components/QuizListView.js
+++ src/components/QuizListView.js
@@ -4,9 +4,14 @@ import {Link} from 'react-router';
 const QuizListView = (props) => (
   <div>
     <h2>Select quiz</h2>
-    <ul className="quiz-list">
-      {props.listCreator(props.quizzes, props.selectQuiz, props.admin, props.editQuiz)}
-    </ul>
+    <div className="quiz-list-container">
+      <ul className="quiz-list">
+        {props.listCreator(props.quizzes, props.selectQuiz, props.admin, props.editQuiz)}
+      </ul>
+      <ul className="passed">
+        {props.passedList(props.quizzes, props.passed)}
+      </ul>
+    </div>
     {props.admin && <button onClick={() => {props.editQuiz()}}>Create Quiz</button>}
   </div>
 );
---
+++ src/utils/passedList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function passedList(quizzes, passed) {
+  let p;
+  return quizzes.map((quiz, index) => {
+    p = passed.filter((item) => {
+      return item.of === quiz._id;
+    });
+    console.log("passed?: ", p);
+    if(p.length > 0)return <li key={index}><svg viewBox="0 0 24 24" preserveAspectRatio="xMidYMid meet" fit><g><path d="M9 16.17l-4.17-4.17-1.42 1.41 5.59 5.59 12-12-1.41-1.41z"></path></g></svg></li>
+    return <li key={index}>false</li>
+  })
+};
