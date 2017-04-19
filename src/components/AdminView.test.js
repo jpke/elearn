120caa4d9bee08bdc73e5bc10a64aea0e89a6417
jpke <j.patrick.earnest@gmail.com>
2017-04-19 17:31:36 -0400

    answerlist callback tests passing

--- src/components/AdminView.test.js
+++ src/components/AdminView.test.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-import {shallow, mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import AdminView from './AdminView';
 import listUsers from '../utils/listUsers';
--- src/components/AnswerList.test.js
+++ src/components/AnswerList.test.js
@@ -3,26 +3,64 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
 
 import AnswerList from './AnswerList';
+
+let selectAnswer, answers;
+beforeEach(() => {
+  selectAnswer = jest.fn();
+  answers = [
+    {
+      _id: "0001",
+      answer: "no current answer",
+      correct: false
+    },
+    {
+      _id: "0002",
+      answer: "no current answer true",
+      correct: true
+    }
+  ];
+})
 
 it('renders correctly', () => {
   const tree = renderer.create(
     <AnswerList
       idSelected={""}
       itemSelected={1}
-      answers={[
-        {
-          answer: "no current answer",
-          correct: false
-        },
-        {
-          answer: "no current answer true",
-          correct: true
-        }
-      ]}
-      selectAnswer={jest.fn()}
+      answers={answers}
+      selectAnswer={selectAnswer}
       />
   ).toJSON();
   expect(tree).toMatchSnapshot();
+})
+
+it('li click calls selectAnswer callback with selected li data', () => {
+  const simulatedEvent = {
+    target: {
+      id: answers[0]._id,
+      textContent: answers[0].answer
+    }
+  };
+  const simulatedEventTwo = {
+    target: {
+      id: answers[1]._id,
+      textContent: answers[1].answer
+    }
+  };
+  const wrapper = shallow(
+    <AnswerList
+      idSelected={""}
+      itemSelected={1}
+      answers={answers}
+      selectAnswer={selectAnswer}
+      />
+  );
+  expect(selectAnswer).not.toHaveBeenCalled()
+  wrapper.find('li').first().simulate('click', simulatedEvent)
+  expect(selectAnswer).toHaveBeenCalledWith(simulatedEvent.target.textContent, simulatedEvent.target.id, 0)
+
+  wrapper.find('li').last().simulate('click', simulatedEventTwo)
+  expect(selectAnswer).toHaveBeenCalledWith(simulatedEventTwo.target.textContent, simulatedEventTwo.target.id, 1)
 })
